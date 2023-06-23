9573a20191f2cb2c968929466aca3a223b713885
emonsingha209 <emonsingha209@gmail.com>
2023-06-23 23:36:37 +0600

    align fixed

--- src/components/Pages/project.js
+++ src/components/Pages/project.js
@@ -7,7 +7,7 @@ function Project() {
       id='projects'
       className='mx-0 md:mx-4 p-4 md:px-0 md:pt-20 md:pb-4 h-full'
     >
-      <div className='text-yellow-400 font-bold text-2xl md:text-4xl text-center mb-4'>
+      <div className='text-yellow-400 font-bold text-2xl md:text-4xl text-center mb-2 md:mb-4'>
         <h2>PROJECTS</h2>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 content-stretch p-4 gap-4 h-full'>
