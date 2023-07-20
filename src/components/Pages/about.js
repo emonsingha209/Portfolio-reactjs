6aa8b1708fb36b8741182aa3cec4002bbb191241
emonsingha209 <emonsingha209@gmail.com>
2023-07-20 23:46:52 +0600

    design added 2.0

--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -31,7 +31,7 @@ function About() {
         </div>
       </div>
       <div className='flex flex-col items-center lg:w-1/3 h-full md:mr-4'>
-        <div className='flex items-center mt-6 text-white text-2xl md:text-4xl font-bold'>
+        <div className='flex items-center mt-6 text-white text-3xl md:text-4xl font-bold'>
           <h2>MY STACKS</h2>
         </div>
         <div className='grid grid-cols-3 text-center text-base md:text-2xl font-bold text-yellow-500 m-4 mt-0 lg:m-2 lg:w-full h-fit'>
