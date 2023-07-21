aecb3cffb53c2511ca0f0e89ce9b0396de604165
emonsingha209 <emonsingha209@gmail.com>
2023-07-22 03:26:59 +0600

    button design 3.0

--- src/components/Button/linkButton.js
+++ src/components/Button/linkButton.js
@@ -3,8 +3,8 @@ function LinkButton(props) {
   return (
     <div>
       <div
-        className='bg-yellow-500 w-fit rounded-lg ring-2 ring-yellow-500 ring-offset-2 ring-offset-gray-800 
-            hover:bg-yellow-600 hover:ring-yellow-600'
+        className='bg-navColor w-fit rounded-lg ring-2 ring-navColor ring-offset-2 ring-offset-gray-800 
+            hover:bg-blue-600 hover:ring-blue-600 text-white'
       >
         <button className='cursor-pointer'>{props.Link}</button>
       </div>
