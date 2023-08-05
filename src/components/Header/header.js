05dfde06e95211483e2304341eb6cf4d862626d9
emonsingha209 <emonsingha209@gmail.com>
2023-08-05 22:54:49 +0600

    logo added

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,14 +2,16 @@ import React from 'react';
 import NavBar from '../NavBar/navbar';
 import NavControl from '../NavBar/navToggle';
 import { Link } from 'react-scroll';
+import logo from '../../images/emonsinghaLogo.png';
 
 function Header() {
   return (
     <div>
       <div className='fixed top-0 left-0 right-0 w-auto h-16 bg-gray-950 text-white flex justify-between'>
-        <div className='font-extrabold text-2xl cursor-pointer flex items-center justify-center ml-4'>
+        <div className='font-extrabold text-2xl cursor-pointer flex items-center justify-center ml-4 md:ml-6'>
           <Link to='intro' smooth={true} duration={500}>
             <div className='flex gap-2'>
+              <img className='w-8 h-8 rounded-full' src={logo} alt='logo'/>
               <h1 className='text-blue-500'>EMON</h1>
               <h1 className='text-yellow-500'>SINGHA</h1>
             </div>
