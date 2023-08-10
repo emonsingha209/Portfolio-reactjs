7b56736e5acdfb79f37383ca8b59b3135db8e641
emonsingha209 <emonsingha209@gmail.com>
2023-08-11 03:24:50 +0600

    basic seo

--- src/App.js
+++ src/App.js
@@ -7,6 +7,21 @@ function App() {
     <div>
       <Header />
       <HomePage />
+      <script type="application/ld+json">
+        {`
+          {
+            "@context": "http://schema.org",
+            "@type": "Portfolio",
+            "name": "Emon Singha",
+            "url": "https://emonsingha.netlify.app/",
+            "description": "Welcome to my portfolio website! I'm Emon Singha, a passionate web 
+            developer with expertise in creating stunning and user-friendly websites. 
+            Explore my work and projects in web development, including responsive designs 
+            and modern technologies.",
+            "logo": "https://i.postimg.cc/NGCBwSyb/emonsingha-Logo.png"
+          }
+        `}
+      </script>
     </div>
   );
 }
