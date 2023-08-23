1d68ed358ef87d4702f3bfc3f75a2f2e484af79c
emonsingha209 <emonsingha209@gmail.com>
2023-08-24 00:01:18 +0600

    svg added

--- src/Pages/contact.js
+++ src/Pages/contact.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useState } from "react";
+import emailjs from "@emailjs/browser";
+import Loading from "../assets/svg/Rolling.svg";
 
 function Contact() {
   const [isEmailSent, setIsEmailSent] = useState(false);
   const [isError, setIsError] = useState(false);
-  const [nameError, setNameError] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [messageError, setMessageError] = useState('');
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [messageError, setMessageError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const validateEmail = (email) => {
@@ -23,29 +24,29 @@ function Contact() {
     let flag = false;
 
     if (!nameInput.value) {
-      setNameError('Please enter your name');
+      setNameError("Please enter your name");
       flag = true;
     } else {
-      setNameError('');
+      setNameError("");
       flag = false;
     }
 
     if (!emailInput.value) {
-      setEmailError('Please enter your email');
+      setEmailError("Please enter your email");
       flag = true;
     } else if (!validateEmail(emailInput.value)) {
-      setEmailError('Please enter a valid email address');
+      setEmailError("Please enter a valid email address");
       flag = true;
     } else {
-      setEmailError('');
+      setEmailError("");
       flag = false;
     }
 
     if (!messageInput.value) {
-      setMessageError('Message cannot be empty');
+      setMessageError("Message cannot be empty");
       flag = true;
     } else {
-      setMessageError('');
+      setMessageError("");
       flag = false;
     }
 
@@ -71,34 +72,34 @@ function Contact() {
     }
   }
   const inputClass =
-    'p-2 w-full placeholder-pen border-2 border-gray-900 rounded-xl hover:border-gray-800 focus:border-gray-950 outline-none';
-  const errorClass = 'text-center font-medium text-white';
+    "p-2 w-full placeholder-pen border-2 border-gray-900 rounded-xl hover:border-gray-800 focus:border-gray-950 outline-none";
+  const errorClass = "text-center font-medium text-white";
   const confirmClass =
-    'text-center font-semibold text-lg p-2 rounded-md text-white';
+    "text-center font-semibold text-lg p-2 rounded-md text-white";
   return (
     <div
-      id='contact'
-      className='w-full h-full flex items-center justify-center'
+      id="contact"
+      className="flex items-center justify-center w-full h-full"
     >
-      <div className='pt-4 md:pt-16 mb-2 h-auto w-full'>
-        <div className='text-yellow-400 text-center mb-2 md:mb-4'>
-          <h3 className='text-3xl md:text-4xl font-bold'>
+      <div className="w-full h-auto pt-4 mb-2 md:pt-16">
+        <div className="mb-2 text-center text-yellow-400 md:mb-4">
+          <h3 className="text-3xl font-bold md:text-4xl">
             Get in Touch with Me
           </h3>
-          <p className='text-lg md:text-xl mt-2 px-2 italic text-gray-200'>
+          <p className="px-2 mt-2 text-lg italic text-gray-200 md:text-xl">
             Use the form below to send me a message or ask any questions
           </p>
         </div>
-        <div className='w-full flex justify-center'>
+        <div className="flex justify-center w-full">
           <form
             onSubmit={sendEmail}
-            className='mx-4 mb-4 p-4 pt-0 w-96 flex flex-col gap-4 rounded text-pen'
+            className="flex flex-col gap-4 p-4 pt-0 mx-4 mb-4 rounded w-96 text-pen"
           >
             <div>
               <input
-                type='text'
-                name='from_name'
-                placeholder='Name'
+                type="text"
+                name="from_name"
+                placeholder="Name"
                 className={inputClass}
               />
               {isError && nameError && (
@@ -107,9 +108,9 @@ function Contact() {
             </div>
             <div>
               <input
-                type='email'
-                name='reply_to'
-                placeholder='Email'
+                type="email"
+                name="reply_to"
+                placeholder="Email"
                 className={inputClass}
               />
               {isError && emailError && (
@@ -118,8 +119,8 @@ function Contact() {
             </div>
             <div>
               <textarea
-                name='message'
-                placeholder='Message...'
+                name="message"
+                placeholder="Message..."
                 className={`${inputClass} h-32`}
               />
               {isError && messageError && (
@@ -127,12 +128,20 @@ function Contact() {
               )}
             </div>
             <div>
-              <input
-                type='submit'
-                value={isLoading ? 'Sending...' : 'Send Message'}
+              <button
+                type="submit"
                 disabled={isLoading}
-                className='bg-green-700 cursor-pointer w-full p-2 rounded-lg font-semibold text-lg hover:bg-green-800 text-white'
-              />
+                className="w-full p-2 text-lg font-semibold text-white bg-green-700 rounded-lg hover:bg-green-800"
+              >
+                {isLoading ? (
+                  <div className="flex items-center justify-center">
+                    <img src={Loading} alt="Loading" className="w-6 h-6 mr-2" />
+                    Sending...
+                  </div>
+                ) : (
+                  "Send Message"
+                )}
+              </button>
             </div>
             {isEmailSent && (
               <p className={`${confirmClass} bg-navColor`}>
