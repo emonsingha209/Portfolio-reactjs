714676a7dd0540a1bd464662c28878c0dcef41cb
emonsingha209 <emonsingha209@gmail.com>
2023-11-10 02:08:07 +0600

    cv updated

--- src/assets/data/cv.js
+++ src/assets/data/cv.js
@@ -1,2 +1,2 @@
 export const CV =
-  "https://drive.google.com/file/d/1-x-8T3d64ONxev2QwKJavfZ4xW2Vndm2/view?usp=sharing";
+  "https://drive.google.com/file/d/123wkFmo3Eeiwq1afApJvpb9CXq2CmAJD/view?usp=sharing";
